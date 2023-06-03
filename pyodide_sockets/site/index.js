9288b17656cf7b62c46e5b53bc8e583d5db74131
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-03 13:46:54 +1200

    Added some helpful debug info to the sitet

--- pyodide_sockets/site/index.js
+++ pyodide_sockets/site/index.js
@@ -1,4 +1,13 @@
 // <span id="indexjs-loaded"></span>
-// make the span say "index.js loaded"
+// make the span say "index.js loaded {date}"
+document.getElementById("indexjs-loaded").innerHTML =
+  "index.js loaded " + new Date();
 
-document.getElementById("indexjs-loaded").innerHTML = "index.js loaded";
+// add seconds since loadded to the span
+// <p id="seconds-since-loaded"></p>
+let start = new Date();
+
+setInterval(function () {
+  document.getElementById("seconds-since-loaded").innerHTML =
+    "seconds since loaded: " + Math.floor((new Date() - start) / 1000);
+}, 1000);
