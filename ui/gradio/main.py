5730733aaf356ef1cc855cfa0388f6df94de6851
Jerome Swannack <jeromeswannack@gmail.com>
2023-06-05 21:23:40 +1200

    Finished gradio tutorial

--- ui/gradio/main.py
+++ ui/gradio/main.py
@@ -1,9 +1,31 @@
+import numpy as np
 import gradio as gr
 
 
-def greet(name: str):
-    return "Hello " + name + "!"
+def flip_text(x):
+    return x[::-1]
 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+def flip_image(x):
+    return np.fliplr(x)
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("## Flip stuff")
+    with gr.Tab("Flip Text"):
+        text_input = gr.Textbox()
+        text_output = gr.Textbox()
+        text_button = gr.Button("flip")
+    with gr.Tab("Flip Image"):
+        with gr.Row():
+            image_input = gr.Image()
+            image_output = gr.Image()
+        image_button = gr.Button("flip")
+
+    with gr.Accordion("Advanced Options"):
+        gr.Markdown("# Extra Options\ntbd")
+
+    text_button.click(flip_text, inputs=text_input, outputs=text_output)
+    image_button.click(flip_image, inputs=image_input, outputs=image_output)
+
 demo.launch()
